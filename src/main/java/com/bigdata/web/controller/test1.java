b8b5eeadb0dcf848e50012725f960d2f9644fe4b
chenxi <chenx2630@foxmail.com>
2019-05-29 16:21:10 +0800

    add

---
+++ src/main/java/com/bigdata/web/controller/test1.java
@@ -0,0 +1,12 @@
+package com.bigdata.web.controller;
+
+/**
+ * @author Administrator
+ * @date 2019/5/29 16:19
+ * @description
+ */
+public class test1 {
+    public static void main(String[] args){
+      System.out.println("122342");
+    }
+}
